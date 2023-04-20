4573b90b39a4f5a3caeb929944b4c17930deab0c
[AxelFONTEYNE] <[axfonteyne3@gmail.com]>
2023-04-20 17:17:26 +0200

    exercice1

--- src/exercice/exercice.java
+++ src/exercice/exercice.java
@@ -1,5 +1,25 @@
 package exercice;
 
 public class exercice {
-
+	public void exercice1(String prenom) {
+		char premiereLettre = prenom.charAt(0);
+		int asciiPremiereLettre = premiereLettre;
+		if(asciiPremiereLettre >= 97 && asciiPremiereLettre <= 122) {
+			asciiPremiereLettre = asciiPremiereLettre - 32;
+			prenom = prenom.substring(1);
+			premiereLettre = (char)asciiPremiereLettre;
+			System.out.println("Hello, " + premiereLettre + prenom + "");
+		}else if(asciiPremiereLettre <= 64 || asciiPremiereLettre >= 123 || (asciiPremiereLettre <= 140 && asciiPremiereLettre >= 91)) {
+			System.out.println("Erreur dans le prénom");
+		}
+		else {
+			System.out.println("Hello, " + prenom + "");
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		exercice1("bob");
+	}
+    
 }
---
+++ test/exerciceTest/test.java
@@ -0,0 +1,9 @@
+package exerciceTest;
+//import exercice.exercice;
+
+public class test {
+	public static void main(String[] args) {
+		//exercice.exercice1("bob");
+	}
+
+}
